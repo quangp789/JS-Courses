db246c2b900bd6e8af6162a277e6af6f54430edf
Quang Phong <quangphong@Quangs-MacBook-Pro.local>
2020-05-22 15:49:44 -0500

    Basic Operators and Precedence

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -29,6 +29,7 @@ var function = 23;
 * Variable Mutation and type coercion
 */
 
+/*
 var firstName = 'Quang';
 var age = 25;
 
@@ -49,12 +50,75 @@ alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + is
 
 var lastName = prompt('What is his last name?'); // Prompt - pop up will allow you to enter a last name which stores into the variable
 console.log(firstName + ' ' + lastName);
+*/
+
+/************************
+* Basic Operators
+*/
+
+/*
+var thisYear, yearQuang, yearMark, ageQuang, ageMark; // You can declare multiple variables on one line
+thisYear = 2020;
+ageQuang = 25;
+ageMark = 33;
+
+//Math Operators
+yearQuang = thisYear - 25; // Calculate the year John was born
+yearMark = thisYear - 33;
+
+console.log(yearQuang); // Display the year Quang was born
+
+console.log(thisYear + 2);
+console.log(thisYear * 2);
+console.log(thisYear / 10);
+
+// Logical Operators
+var quangOlder = ageQuang > ageMark;
+console.log(quangOlder); // Log the varible to see if Quang is older than Mark
+
+// Typeof Operators
+console.log(typeof quangOlder); // Typeof - tells us the type of the input variable
+console.log(typeof ageQuang); // Displays a number
+console.log(typeof 'This is a string') // Displays a string
+var x;
+console.log(typeof x); // Displays undefined b/c nothing was assign to the variable
+*/
+
+/************************
+* Basic Precedence
+*/
+
+var thisYear, yearQuang, legalAge
+
+thisYear = 2020;
+yearQuang = 1994;
+legalAge = 21;
 
 
+// Multiple operators
+var isFullAge = thisYear - yearQuang >= legalAge; // Asking if Quang is older than the legal age. '-' takes a higher precedence than '>='
+console.log(isFullAge); // Log the console. Displays true.
 
+// Grouping
+var ageQuang = thisYear - yearQuang;
+var ageMark = 33;
+var averageAge  = (ageQuang + ageMark) / 2; //Due to PEMDAS the 2 ages musted by grouped first in order to calculate the average age.
+console.log(averageAge);
 
+// Multiple Assignments
+var x, y;
+x =  y = (3 + 5) * 4 - 6; // 8 * 4 - 6 = 32 - 6 = 26 // Assignment operator works from right to left. Which is why x and y equal to 26
+console.log(x, y); // You can log multiple variables
 
-
+// Additional Operators
+x *= 2; // means the exact same thing as x = x * 2
+console.log(x);
+x += 10;
+console.log(x);
+x++; // This operator adds one unit to the variable
+console.log(x);
+x--;
+console.log(x);
 
 
 
