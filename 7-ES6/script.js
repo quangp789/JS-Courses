3c6712a3dc3213e4c18299751b2785d84666031b
Quang Phong <quangphong@Quangs-MacBook-Pro.local>
2020-07-27 20:15:36 -0500

    Arrays / Spread operator / Rest parameters / Default parametes / Maps / Classes

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -15,7 +15,7 @@
 
 
 	// ES5
-	function driversLicense5(passedTest) {
+	function driversLicense5(passedTest) { // passing in an boolean arguement
 		if (passedTest) {
 			var firstName = 'john';
 			var yearOfBirth = 1990;
@@ -55,8 +55,12 @@
 
 // Block and IIFEs
 
+	// ES5 IIFE
+	(function() {
+		var c = 3;
+	})();
 
-	// To make an IIFE all you have to do is create a block using const and let
+	// ES6 IIFE - To make an IIFE all you have to do is create a block using const and let
 	{
 		const a = 1;
 		let b = 2;
@@ -65,13 +69,6 @@
 
 	console.log(a + b); // This will failed due to block scope with "let" and "const"
 	console.log(c); // This will work cause "var is not block scope
-
-
-	// ES5 IIFE
-	(function() {
-		var c = 3;
-	})();
-
 
 // Strings
 
@@ -102,7 +99,7 @@
 	const years = [1945, 1989, 1923, 1970, 1994];
 
 	// ES5
-	// Using the map method, it will pass every element to your function in which will return the result
+	// Using the map method, it will pass every element to your function in which will return the result in a new array
 	var ages5 = years.map(function(element) {
 		return 2020 - element;
 	});
@@ -175,7 +172,7 @@
 		}
 	}
 	box6.clickMe();
-	*/
+
 
 	// ES5 Function Constructor
 	function Person(name) {
@@ -214,8 +211,303 @@
 	new Person('Quang').myFriends6(friends);
 
 
+// Destructuring
+
+	// ES5
+	var john = ['John', 26];
+	//var name = john[0];
+	//var age = john[1];
+
+	//ES6
+	const [name, year] = ['John', 26];
+	console.log(name);
+	console.log(year);
+
+	const obj = {
+		firstName: 'Quang',
+		lastName: 'Phong',
+	};
+
+	const {firstName, lastName} = obj;
+	console.log(firstName, lastName);
+
+	const {firstName: a, lastName: b} = obj; // You can assign a letter to your object
+	console.log(a, b);
+
+	// Function Destructuring
+	function calcAgeRetirement(year){
+		const age = new Date().getFullYear() - year; // Calculate the age
+		return [age, 65 - age]; // Return your age and years left before retirement
+	}
+
+	const [age2, retirement] = calcAgeRetirement(1994);
+	console.log(age2, retirement);
 
 
+	// Arrays
+	const boxes = document.querySelectorAll('.box') // Select all the CSS with a class of box
+
+	// ES5
+
+	var boxesArr5 = Array.prototype.slice.call(boxes);
+	boxesArr5.forEach(function(cur) {
+		cur.style.backgroundColor = 'dodgerblue';
+	});
+
+
+	// ES6
+	const boxesArr6 = Array.from(boxes);
+	Array.from(boxes).forEach(cur => cur.style.backgroundColor = 'red');
+
+	// ES5 For Loop
+
+	for(var i = 0; i <boxesArr5.length; i++) {
+
+		// If this element has the class name of "box blue"
+		if(boxesArr5[i].className === 'box blue') {
+			continue; // skip this class name and move on to the next element in the array
+		}
+
+		boxesArr5[i].textContent = 'I changed to blue!';
+
+	}
+
+
+	// ES6 For Loop
+	// We can define our current element of the array
+	for (const cur of boxesArr6) {
+		if(cur.className.includes('blue')) {
+			break;
+		}
+		cur.textContent = 'I changed to blue!';
+	}
+
+	// ES5
+	var ages = [12, 13, 17, 19, 9, 26];
+
+	// Return a new array of all the elements that are greater than 18
+	var fullAge = ages.map(function(cur) {
+		return cur >= 18;
+	});
+
+	console.log(fullAge);
+
+	console.log(fullAge.indexOf(true));
+	console.log(ages[fullAge.indexOf(true)]);
+
+	// ES6
+	console.log(ages.findIndex(cur => cur >= 18)); // Returns the first position(index) that is true
+	console.log(ages.find(cur => cur >= 18)); // Returns the first element that is true
+
+
+// Spread Operator
+
+	function addFourAges (a, b, c, d) {
+		return a + b + c + d;
+	}
+
+	var sum1 = addFourAges(12, 14, 22, 12);
+	console.log(sum1);
+
+	// ES5
+	var ages = [12, 14, 22, 12];
+	var sum2 = addFourAges.apply(null, ages); // Call your function and use the apply method on ages variable. Null is the position of the array
+
+	console.log(sum2);
+
+	// ES6
+	const sum3 = addFourAges(...ages); // "..." adds all the element in the array.
+	console.log(sum3);
+
+	// Joining family names
+	const familySmith = ['John', 'Jane', 'Mark'];
+	const familyMiller = ['Mary', 'Bob', 'Ann'];
+
+	const joinFamily = [...familySmith, ...familyMiller];
+	console.log(joinFamily);
+
+	const h = document.querySelector('h1'); // No # or . needed cause its an element and not an ID or class
+	const boxes = document.querySelectorAll('.box');
+
+	const all = [h, ...boxes]; // Combine both h and boxes into 1 array
+	Array.from(all).forEach(cur => cur.style.color = 'purple'); // For each element in the "all" array you want to change the styling to 														 // purple
+
+// Rest Parameters
+
+	function isFullAge5() {
+		//console.log(arguments);
+		var argsArr = Array.prototype.slice.call(arguments); // Change your argument property to an array
+
+		argsArr.forEach(function(cur) {
+
+			var presentYear = new Date().getFullYear(); // Obtain the full year and store into a variable
+
+			console.log((presentYear - cur) >= 18); // Log the year to console with calculations
+		})
+
+	}
+
+	isFullAge5(1990, 1999, 1965, 2016, 1987);
+
+
+	// ES6
+	function isFullAge6(...years) {
+
+		var presentYear = new Date().getFullYear();
+
+		years.forEach(cur => console.log((presentYear - cur) >= 18)); //For each element you pass, you want the boolean result of anything greater than 18
+	}
+
+	isFullAge6(1990, 1999, 1965, 2016, 1987);
+
+
+	// Rest perameter continue..
+	function isFullAge5(limit) {
+
+		var argsArr = Array.prototype.slice.call(arguments, 1); // Starts at position 1. Function will skip element 0.
+
+		argsArr.forEach(function(cur) {
+
+			var presentYear = new Date().getFullYear();
+
+			console.log((presentYear - cur) >= limit);
+		})
+
+	}
+
+	isFullAge5(21, 1990, 1999, 1965, 2016, 1987);
+
+
+	// ES6
+	function isFullAge6(limit, ...years) { // You can pass as much years as you want
+
+		var presentYear = new Date().getFullYear();
+
+		years.forEach(cur => console.log((presentYear - cur) >= limit)); // Refers to position 0 in the arguments passed
+	}
+
+	isFullAge6(16, 1990, 1999, 1965, 2016, 1987);
+
+
+// Default Parameters
+
+	function phongPerson(firstName, yearOfBirth, lastName,  nationality) {
+
+		lastName === undefined ? lastName = 'Phong' : lastName; // If lastname is undefined then its = to Phong else its lastName
+		nationality === undefined ? nationality = 'American' :lastName;
+
+
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.yearOfBirth = yearOfBirth;
+		this.nationality = nationality;
+	}
+
+
+	//ES6
+	// Default parameters can be defined within the argument
+	function phongPerson(firstName, yearOfBirth, lastName = 'Phong', nationality = 'American') {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.yearOfBirth = yearOfBirth;
+		this.nationality = nationality;
+	}
+
+	var quang = new phongPerson('Quang', 1990); //JS will assigned undefined to the other parameters
+
+	var jane = new phongPerson('Jane', 1994, 'Smith', 'Spanish');
+
+
+// Maps
+
+	const question = new Map();
+	question.set('question', 'What is the official name of the latest major JS version?'); // Key-Value pair of your new map
+
+	// Answer Choices
+	question.set(1, 'ES5');
+	question.set(2, 'ES6');
+	question.set(3, 'ES2015');
+	question.set(4, 'ES7');
+
+	// Add correct answer
+	question.set('correct', 3);
+	question.set(true, 'Correct Answer!');
+	question.set(false, 'Wrong Answer! Please try again.');
+
+	// Retrieve data from this map
+	// console.log(question.size); // Size will check the length of your map
+
+	if(question.has(4)) {
+		// question.delete(4); // This will remove your key number 4(4th choice)
+		console.log('Answer 4 is here')
+	};
+
+	// question.clear(); // Deletes all your map objects
+
+	// Loops can also work on maps
+	question.forEach((value, key) => console.log(`This is ${key}, and it's set to ${value}`));
+
+
+
+	// Stores the key and value pair into seperate values. Print the value if the key is a number
+	console.log(question.get('question')); // This will get the question of the object
+
+	for (let [key, value] of question.entries()) {
+		if (typeof(key) === 'number') {
+			console.log(`Answer ${key}: ${value}`);
+		}
+	}
+
+	const ans = parseInt(prompt('Write the correct answer')); // Change the input from string to a int
+
+	console.log(question.get(ans === question.get('correct'))); // If the input is the same as the answer then the expression will be true
+	*/
+
+// Classes
+
+	//ES5
+	// create a function constructor
+	var Person5 = function(name, yearOfBirth, job) {
+		this.name = name;
+		this.yearOfBirth = yearOfBirth;
+		this.job = job;
+	}
+
+	// create a calculateAge mathod
+	Person5.prototype.calculateAge = function() {
+		var age = new Date().getFullYear - this.yearOfBirth;
+		console.log(age);
+	}
+
+	// display your persons age
+	var quang5 = new Person5 ('Quang', 1994, 'IT');
+
+
+	//ES6
+	class Person6 {
+		constructor (name, yearOfBirth, job) {
+			this.name = name;
+			this.yearOfBirth = yearOfBirth;
+			this.job = job;
+		}
+
+		// Functions can directly add to the class
+		calculateAge() {
+			var age = new Date().getFullYear() - this.yearOfBirth;
+			console.log(age);
+		}
+
+		// Static classes
+		static greeting() {
+			console.log('Hey there!');
+		}
+	}
+
+	const quang6 = new Person6('Quang', 1994, 'IT');
+
+	Person6.greeting();
+
+	quang6.calculateAge();
 
 
 
